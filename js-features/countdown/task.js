7b6285d76b1e17627e8ebcf2cd941a528562534e
Mariya Zamyatina <maria.net@mail.ru>
2023-07-02 14:22:41 +0700

    fix task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,12 +2,17 @@
     const date = new Date().toLocaleTimeString(); //текущее время hh:mm:ss
     let seconds = Number(date.slice(-2));
     const p = document.getElementById('timer');
-    p.textContent = `00:00:${date.slice(-2)}`
-    setInterval(() => {
+    p.textContent = `00:00:${date.slice(-2)}`;
+
+    let timerId = setInterval(() => {
     seconds = seconds - 1;
-    if (seconds === 0) {return alert('Вы победили в конкурсе!')};
-    if (seconds < 0) {return};
-    if (seconds <10) {str = '00:00:' + '0' + String(seconds)}
-    else {str = '00:00:' + String(seconds)};
+    console.log(seconds);
+    if (seconds === 0) {
+        clearTimeout(timerId);
+        return alert('Вы победили в конкурсе!')};
+    if (seconds < 10) {
+        str = '00:00:' + '0' + String(seconds)}
+    else {
+        str = '00:00:' + String(seconds)};
     p.textContent = `${str}`},1000);
 })()
