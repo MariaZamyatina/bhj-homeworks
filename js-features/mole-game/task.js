c5dacf2737a93282976e4a774bf529913c551892
Mariya Zamyatina <maria.net@mail.ru>
2023-07-02 15:59:13 +0700

    change task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,28 +3,28 @@ miss = document.getElementById('lost').textContent,
 counter = document.getElementById('dead').textContent;
 
 for (let i = 1; i <= 9; i++) {
-    hole = getHole(i);
-    hole.addEventListener('click', () => {
-      if (hole.className.includes('hole_has-mole')) { 
-        counter++;
-        document.getElementById('dead').textContent = counter; }
-      else { 
-        miss++;
-        document.getElementById('lost').textContent = miss;
-       }
-      if (counter === 10) {
-        document.getElementById('lost').textContent = 0;
-        document.getElementById('dead').textContent = 0;
-        counter = 0;
-        miss = 0;
-        alert("Вы победили!");
-       }
-      else if (miss === 5) {
-        document.getElementById('lost').textContent = 0;
-        document.getElementById('dead').textContent = 0;
-        miss = 0;
-        counter = 0;
-        alert("Вы приграли!");
-       }
+  let hole = getHole(i);
+  hole.addEventListener('click', () => {
+    if (hole.className.includes('hole_has-mole')) { 
+      counter++;
+      document.getElementById('dead').textContent = counter; }
+    else { 
+      miss++;
+      document.getElementById('lost').textContent = miss;
+      };
+    if (counter === 10) {
+      changeStatus('Вы победили!');
+      }
+    else if (miss === 5) {
+      changeStatus('Вы приграли!');
+      };
   });
-}
+};
+
+function changeStatus(text) {
+  document.getElementById('lost').textContent = 0;
+  document.getElementById('dead').textContent = 0;
+  miss = 0;
+  counter = 0;
+  alert(text);
+};
