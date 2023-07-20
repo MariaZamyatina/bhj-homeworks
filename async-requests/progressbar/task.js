964e62ae252814897fca75ce30b7b6aab09e54ff
Mariya Zamyatina <maria.net@mail.ru>
2023-07-20 12:56:57 +0700

    fix

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -7,17 +7,46 @@ function loadFile() {
 
     const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('readystatechange', () => {
-      
-      const progress = document.getElementById( 'progress' );
-      let timerId = setInterval(() => {
-        if (xhr.readyState === xhr.DONE) {
-            progress.value = 1.0; 
+    let loaded = 0;
+   
+    let percent = 0;
+
+    xhr.upload.onprogress = function(event) {
+
+      const timerId = setInterval(() => {
+        // console.log(`Отправлено ${event.loaded} из ${event.total} байт`);
+        
+        loaded = loaded + event.loaded;
+
+        // процент
+        percent = Math.round((event.loaded / event.total) * 100) ;
+
+        console.log(`Отправлено ${event.loaded} % из ${event.total} байт`);
+
+        
+
+        if (xhr.status === 201) {
+          
+            console.log('status',xhr.status);
             clearInterval(timerId);
-        } 
-        else progress.value += 0.05;    
-       }, 100);
-    })
+          };
+          //         progress.value = 1.0; 
+          //         clearInterval(timerId); 
+      },3000)
+     
+    };
+
+    // xhr.addEventListener('readystatechange', () => {
+      
+    //   const progress = document.getElementById( 'progress' );
+    //   let timerId = setInterval(() => {
+    //     if (xhr.readyState === xhr.DONE) {
+    //         progress.value = 1.0; 
+    //         clearInterval(timerId);
+    //     } 
+    //     else progress.value += 0.05;    
+    //    }, 100);
+    // })
 
     xhr.open('POST','https://students.netoservices.ru/nestjs-backend/upload');
 
